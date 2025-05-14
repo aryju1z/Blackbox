b365f8133d2bdfd76ec80d35756de9a5bed28c18
Ary Juiz <apaschoal@minsait.com>
2025-05-14 12:22:01 -0300

    Atualizações finais no projeto Gaudenci School

--- gaudenci_school_project/scripts/decorative-elements.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Arquivo limpo: código relacionado aos elementos decorativos foi removido, pois não está mais em uso.
-});
--- gaudenci_school_project/scripts/mobile-menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Código para o menu cascata móvel
-    const mobileMenuButton = document.getElementById('mobile-menu-button');
-    const mobileMenu = document.getElementById('mobile-menu');
-
-    mobileMenuButton.addEventListener('click', () => {
-        mobileMenu.classList.toggle('open');
-    });
-});
